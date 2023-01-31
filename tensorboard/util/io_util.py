128c07007a83a44cb42739efc4bddc50d23a23cd
Joost van Doorn <joost@kaiko.ai>
2023-01-31 13:00:56 +0100

    Add az:// cloud path support

--- tensorboard/util/io_util.py
+++ tensorboard/util/io_util.py
@@ -17,4 +17,4 @@
 
 def IsCloudPath(path):
     """Checks whether a given path is Cloud filesystem path."""
-    return path.startswith("gs://") or path.startswith("s3://")
+    return path.startswith("gs://") or path.startswith("s3://") or path.startswith("az://")
